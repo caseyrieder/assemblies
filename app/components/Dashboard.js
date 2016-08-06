46a4b0716667fbacbeb059d3b4e50b7be541ae82
Casey Rieder <casey.rieder@gmail.com>
2016-08-06 08:49:23 -0400

    added tabbar and wired to 3 empty views

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -1,65 +1,27 @@
-// redesign the Dashboard to act as a Dashboard
 import React, { Component } from 'react'
-// import { View, Text, TouchableOpacity } from 'react-native'
-// import Icon from 'react-native-vector-icons/Ionicons'
-// import NavigationBar from 'react-native-navbar'
-// import Colors from '../styles/colors'
-// import { globals } from '../styles'
-
-// import tab bar component & related icon
 import { TabBarIOS } from 'react-native'
 import { TabBarItemIOS } from 'react-native-vector-icons/Ionicons'
-// import 3 views to link wihtin tabbar
+
 import ActivityView from './activity/ActivityView'
 import MessagesView from './messages/MessagesView'
 import ProfileView from './profile/ProfileView'
 
 class Dashboard extends Component {
   constructor() {
-    super();
-    // this.goBack = this.goBack.bind(this);
-    // this.visitLanding = this.visitLanding.bind(this);
+    super()
     this.state = {
       selectedTab: 'Activity'
     }
   }
-  //
-  // visitLanding() {
-  //   this.props.navigator.push({
-  //     name: 'Landing'
-  //   });
-  // }
-  //
-  // goBack() {
-  //   this.props.navigator.pop();
-  // }
 
   render() {
-    // let titleConfig = {title: 'Dashboard', tintColor: 'white'};
     return (
-      // <View style={globals.flexContainer}>
-      //   <NavigationBar
-      //     title={titleConfig}
-      //     tintColor={Colors.brandPrimary}
-      //     leftButton={<BackButton handlePress={this.goBack} />}
-      //   />
-      //   <View style={globals.flexCenter}>
-      //     <Text style={globals.h2}>
-      //       This is the Dashboard
-      //     </Text>
-      //     <TouchableOpacity onPress={this.visitLanding}>
-      //       <Text>
-      //         Go to the Landing Page
-      //       </Text>
-      //     </TouchableOpacity>
-      //   </View>
-      // </View>
       <TabBarIOS>
         <TabBarItemIOS
           title='Activity'
           selected={this.state.selectedTab === 'Activity'}
           iconName='ios-pulse'
-          onPress={() => this.setState({ seletedTab: 'Activity'})}
+          onPress={() => this.setState({ selectedTab: 'Activity'})}
         >
           <ActivityView />
         </TabBarItemIOS>
@@ -67,7 +29,7 @@ class Dashboard extends Component {
           title='Messages'
           selected={this.state.selectedTab === 'Messages'}
           iconName='ios-chatboxes'
-          onPress={() => this.setState({ seletedTab: 'Messages'})}
+          onPress={() => this.setState({ selectedTab: 'Messages'})}
         >
           <MessagesView />
         </TabBarItemIOS>
@@ -75,7 +37,7 @@ class Dashboard extends Component {
           title='Profile'
           selected={this.state.selectedTab === 'Profile'}
           iconName='ios-person'
-          onPress={() => this.setState({ seletedTab: 'Profile'})}
+          onPress={() => this.setState({ selectedTab: 'Profile'})}
         >
           <ProfileView />
         </TabBarItemIOS>
