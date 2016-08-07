de373f4f35c994992a939570de813ae486e974f2
Casey Rieder <casey.rieder@gmail.com>
2016-08-07 11:32:42 -0400

    Added callback to update most recent message into conversations collection from messages collection

---
+++ api/resources/messages/post.js
@@ -0,0 +1,28 @@
+console.log('MESSAGE CREATED', this);
+var text = this.text;
+var senderId = this.senderId;
+var recipientId = this.recipientId;
+var createdAt = this.createdAt;
+
+dpd.conversations.get({
+    $or: [
+        {user1Id: senderId, user2Id: recipientId},
+        {user2Id: senderId, user1Id: recipientId}
+    ]
+})
+.then(function(data){
+   console.log('DATA', data);
+   if (data.length) {
+       dpd.conversations.put(data.id, {
+           lastMessageText: text,
+           lastMessageDate: createdAt
+       });
+   } else {
+       dpd.conversations.post({
+          user1Id: senderId,
+          user2Id: recipientId,
+           lastMessageText: text,
+           lastMessageDate: createdAt
+       });
+   }
+});
